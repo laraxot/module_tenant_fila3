d424f0776ca9d3782ff61d543327d8f00f208c22
Malebestia <vair81@gmail.com>
2023-08-21 17:28:49 +0200

    up

--- Config/xra.php
+++ Config/xra.php
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 <?php
 
 declare(strict_types=1);
@@ -11,21 +9,3 @@ return [
     'primary_lang' => 'it',
     'pub_theme' => 'One',
 ];
-=======
-=======
->>>>>>> 30db2b8 (up)
-<?php
-
-
-return [
-    'adm_theme' => 'Clever',
-    'enable_ads' => '1',
-    'main_module' => 'PFed',
-    'primary_lang' => 'it',
-    'pub_theme' => 'One',
-<<<<<<< HEAD
-];
->>>>>>> 8d95d96 (up)
-=======
-];
->>>>>>> 30db2b8 (up)
--- docs/listeners/GenerateSitemap.php
+++ docs/listeners/GenerateSitemap.php
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 <?php
 
 declare(strict_types=1);
@@ -36,7 +34,8 @@ class GenerateSitemap
             })->each(
                 function ($path) use ($baseUrl, $sitemap) {
                     $sitemap->addItem(rtrim($baseUrl, '/') . $path, time(), Sitemap::DAILY);
-                });
+                }
+            );
 
         $sitemap->write();
     }
@@ -46,56 +45,3 @@ class GenerateSitemap
         return Str::is($this->exclude, $path);
     }
 }
-=======
-=======
->>>>>>> 30db2b8 (up)
-<?php
-
-declare(strict_types=1);
-
-namespace App\Listeners;
-
-use Illuminate\Support\Str;
-use samdark\sitemap\Sitemap;
-use TightenCo\Jigsaw\Jigsaw;
-
-class GenerateSitemap
-{
-    protected $exclude = [
-        '/assets/*',
-        '*/favicon.ico',
-        '*/404',
-    ];
-
-    public function handle(Jigsaw $jigsaw)
-    {
-        $baseUrl = $jigsaw->getConfig('baseUrl');
-
-        if (! $baseUrl) {
-            echo "\nTo generate a sitemap.xml file, please specify a 'baseUrl' in config.php.\n\n";
-
-            return;
-        }
-
-        $sitemap = new Sitemap($jigsaw->getDestinationPath().'/sitemap.xml');
-
-        collect($jigsaw->getOutputPaths())
-            ->reject(function ($path) {
-                return $this->isExcluded($path);
-            })->each(
-function ($path) use ($baseUrl, $sitemap) {
-                $sitemap->addItem(rtrim($baseUrl, '/').$path, time(), Sitemap::DAILY);
-            });
-
-        $sitemap->write();
-    }
-
-    public function isExcluded($path)
-    {
-        return Str::is($this->exclude, $path);
-    }
-}
-<<<<<<< HEAD
->>>>>>> 8d95d96 (up)
-=======
->>>>>>> 30db2b8 (up)
